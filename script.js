84571af5ae3a5505aa294dcf38d19a1f4a94efaa
SamuelJones24 <samdude24@outlook.com>
2023-01-05 12:31:16 -0600

    issue with server hopefully this fixes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Import Three.js
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from "./three.module.js";
 // Create the scene
 const scene = new THREE.Scene();
 
